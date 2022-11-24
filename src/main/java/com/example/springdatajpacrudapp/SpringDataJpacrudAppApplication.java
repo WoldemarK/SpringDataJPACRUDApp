96994bdb86b10592af5ac0d082faf442e0cde9ab
WoldemarK <kovtynov.vladimir@gmail.com>
2022-11-24 21:42:26 +0300

    new

---
+++ src/main/java/com/example/springdatajpacrudapp/SpringDataJpacrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpacrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpacrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpacrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdatajpacrudapp/SpringDataJpacrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpacrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpacrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
